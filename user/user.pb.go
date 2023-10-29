2dda80a94062817135b653a2054f2af848f27f58
朱广锋 <12827061+zgf1163391068@user.noreply.gitee.com>
2023-10-29 20:38:57 +0800

    go mod tidy

--- user/user.pb.go
+++ user/user.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.19.4
 // source: user.proto
 
-package proto
+package user
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
--- user/user_grpc.pb.go
+++ user/user_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v3.19.4
 // source: user.proto
 
-package proto
+package user
 
 import (
 	context "context"
